4bae5d528f43c0b83c4c9a6b677a006c1e67f628
LeHoangKhang3g <khangxyz3g@gmail.com>
2021-11-02 13:35:44 +0700

    Update

---
+++ mail_app/lib/interface_mail.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Interface_Mail extends StatefulWidget{
+  
+}
---
+++ mail_app/lib/sign_in.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget{
+
+}
+
+class SignFailed extends StatelessWidget{
+
+}
+
+class LoadingSignIn extends StatefulWidget{
+  
+}
